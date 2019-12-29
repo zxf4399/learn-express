dbae576665c0a8b4b6531cd1fd9f910c3a6ac18e
zxf4399 <zxf4399@gmail.com>
2019-12-29 10:31:36 +0800

    fix: add express-app api url

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send({ data: { name: 'zxf4399', age: 27, sex: 1 } });
 });
 
 module.exports = router;
